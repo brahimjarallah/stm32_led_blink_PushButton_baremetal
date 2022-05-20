2a33d2553a2cd7764f430fc5b84ee918309009c1
brahimjarallah <brahim.jarallah@gmail.com>
2022-05-20 18:08:41 +0200

    led on when push btn is pressed

--- main.c
+++ main.c
@@ -4,16 +4,16 @@
 // B1 / PA0
 
 int main(void){ 
-		RCC->AHB1ENR |= 0x8000; // enable port D  (for LED PD6)
+		RCC->AHB1ENR |= 1000; // enable port D  (for LED PD6)
 		RCC->AHB1ENR |= 0x1;    // enable port A  (for push button PA0)
 		GPIOD->MODER |= 0x40000000; // make pin D as output (2bits/pin of 32bits)
 	
 		while(1){
-			if(GPIOA->IDR & 0x1){  // if PA0 is hight (push button not pressed)
-				GPIOD->BSRR = 0x80000000u; // turn off Led PD6
+			if(GPIOA->IDR & 0x1){  // if PA0 is hight (push button pressed)
+						GPIOD->BSRR = 0x8000u;  // turn on Led PD6
 			} 
 			else{
-			  GPIOD->BSRR = 0x8000u;  // turn on Led PD6
+						GPIOD->BSRR = 0x80000000u; // turn off Led PD6
 			} 
 		}
 }
